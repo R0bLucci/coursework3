e1363746412a60c98e061b6441ac887499fe01fd
bensmithbortey <ben-smithbortey@hotmail.co.uk>
2016-03-04 17:15:36 +0000

    javadoc for Observable and observer

--- src/benjaminrobert/controller/Observer.java
+++ src/benjaminrobert/controller/Observer.java
@@ -5,6 +5,13 @@ import javax.swing.JTextField;
 
 import benjaminrobert.view.Observable;
 
+/**
+ * Interface - for the observer.
+ * @author benjaminrobert
+ * This method is called whenever the observed object is changed. 
+ * @param subject This is the first paramter to observer interface
+ * @param textField This is the second paramter to observer interface
+ */
 public interface Observer {
 
 	public void update(Observable subject, JButton button, JTextField textField);
--- src/benjaminrobert/view/Observable.java
+++ src/benjaminrobert/view/Observable.java
@@ -5,6 +5,14 @@ import javax.swing.JTextField;
 
 import benjaminrobert.controller.Observer;
 
+
+/**
+ * Interface - for the observable.
+ * @author benjaminrobert
+ * This method is allows an object to be observed.. 
+ * @param subject This is the first paramter to Observable interface
+ * @param textField This is the second paramter to Observable interface
+ */
 public interface Observable {
 
 	public void notifyObservers(JButton button, JTextField textField);
